09c69d319632a5c44f2a4f09b88e2bec23261a5f
valujko <valujko@gmail.com>
2019-03-12 21:40:56 +0300

    test

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input word:");
+            String st =  Console.ReadLine().ToLower();
+            int sum = 0;
+
+            for (int i = 0; i < st.Length; i++)
+            {
+                if (st[i].Equals('a')) sum++; 
+            }
+            Console.WriteLine($"Number of 'a' = {sum} ");
+
+            Console.ReadKey();
+        }
+    }
+}
